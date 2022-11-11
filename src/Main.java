eba621753a863941723f9c28daf4ab8dd4168e81
rus <ruslan.cheremisin@gmail.com>
2022-11-11 21:20:32 +0200

    Задание 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+        Product potato = new Product("Potato");
+        Product carrot = new Product("Carrot");
+        Product onion = new Product("Onion");
+        Product cucumber = new Product("Cucumber");
+        Product cucumber2 = new Product(null);
+
+        ProductList pL = new ProductList();
+        pL.addProduct(potato);
+        pL.addProduct(carrot);
+        pL.addProduct(onion);
+        pL.addProduct(cucumber);
+//        pL.addProduct(cucumber);
+//        pL.addProduct(cucumber2);
+
+        System.out.println(Arrays.toString(pL.getProductList().toArray()));
+
+
     }
 }
